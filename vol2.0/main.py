42a3c708bc53d567d81fd4bf7e33ef3c99579ab2
Leonardo Mtz <119015812+LeoOrVyus@users.noreply.github.com>
2023-05-14 13:16:04 -0600

    Update main.py

--- vol2.0/main.py
+++ vol2.0/main.py
@@ -4,7 +4,7 @@ from chatbot_controller import ChatbotController
 from chatbot_view import ChatbotView
 
 # Ingresa tu clave de API de OpenAI aquí
-api_key = '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'
+api_key = ''
 
 # Crear una instancia del controlador del chatbot y ejecutarlo
 controller = ChatbotController(api_key)
