b8fcb6ff8ba3764640f34a515bb0823d2722f804
Leonardo Mtz <119015812+LeoOrVyus@users.noreply.github.com>
2023-05-14 12:31:45 -0600

    Delete intento 1 directory

--- intento 1/chatbot_view.py
+++
@@ -1,8 +0,0 @@
-class ChatbotView:
-    @staticmethod
-    def get_user_input():
-        return input("Usuario: ")
-
-    @staticmethod
-    def display_response(response):
-        print("Bot:", response)
